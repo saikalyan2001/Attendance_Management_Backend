9cdd5f5360314f4945cfb74bcbec3d28171788c6
saikalyan2001 <52730178+saikalyan2001@users.noreply.github.com>
2025-10-28 16:09:58 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import { login, signup, createSiteIncharge, createSuperAdmin, logout, getMe, getLocations, setPassword, forgotPassword } from '../controllers/authController.js';
+import { 
+  login, 
+  signup, 
+  createSuperAdmin, 
+  logout, 
+  getMe, 
+  getLocations, 
+  resetPassword 
+} from '../controllers/authController.js';
 import { protect, restrictTo } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', protect, restrictTo('admin', 'super_admin'), signup);
-router.post('/create-siteincharge', protect, restrictTo('admin'), createSiteIncharge);
-router.post('/create-superadmin', protect, restrictTo('super_admin'), createSuperAdmin);
+router.post('/create-superadmin', createSuperAdmin);
+router.post('/reset-password', protect, resetPassword);
 router.post('/logout', protect, logout);
 router.get('/me', protect, getMe);
 router.get('/locations', getLocations);
-router.post('/set-password', setPassword);
-router.post('/forgot-password', forgotPassword);
 
 export default router;
