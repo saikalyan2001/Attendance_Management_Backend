410a2390e5e42f880dbcacc7d0a08c43a83ab73a
saikalyan2001 <52730178+saikalyan2001@users.noreply.github.com>
2025-10-28 16:10:45 +0530

    Remove createSiteIncharge route from superAdmin.js

--- routes/superadmin/superAdmin.js
+++ routes/superadmin/superAdmin.js
@@ -2,7 +2,6 @@
 import express from 'express';
 import { protect, restrictTo } from '../../middleware/authMiddleware.js';
 import { getAllUsers, updateUser, deleteUser, fetchSuperAdminDashboard } from '../../controllers/superadmin/superAdminController.js';
-import {  createSiteIncharge } from '../../controllers/authController.js';
 
 const router = express.Router();
 
@@ -13,7 +12,6 @@ router.get('/users', getAllUsers);
 router.put('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
 // router.post('/create-admin', createAdmin);
-router.post('/create-siteincharge', createSiteIncharge);
 router.get('/dashboard', fetchSuperAdminDashboard);
 
 export default router;
